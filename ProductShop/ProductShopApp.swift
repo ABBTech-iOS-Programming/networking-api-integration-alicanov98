b50ff87de6e79888c3e253ffe375a6625a47cd01
alicanov98 <alicanov98@gmail.com>
2026-08-20 11:53:37 +0400

    Initial project setup

---
+++ ProductShop/ProductShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductShopApp.swift
+//  ProductShop
+//
+//  Created by Malik Alijanov on 20.08.26.
+//
+
+import SwiftUI
+
+@main
+struct ProductShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
